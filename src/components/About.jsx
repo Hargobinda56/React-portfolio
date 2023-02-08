0a498dd37511d89be4d759810d2f77b976dd2415
Hargobinda <Mhargobinda@gmail.com>
2023-02-09 04:11:03 +0530

    project done

---
+++ src/components/About.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div name="about" className='w-full h-screen bg-gradient-to-b from-gray-800 to-black text-white'>
+    <div className='max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full'>
+     <div className='pb-8'>
+        <p className='text-4xl font-bold inline border-b-4 border-gray-500'>
+        About
+        </p>
+
+        
+     </div>
+        <p className='text-xl mt-20'> 
+        Hello My Name is Hargobinda i have worked in many project one of my favourite project that i have worked in is Crypto currency Dashboard. 
+        Also i have worked on a Project Flash Card Generator.
+        </p>
+        
+        <br/>
+        <p className='text-xl'>
+        Researching, designing, implementing, 
+        and managing software programs. 
+        Testing and evaluating new programs. 
+        Identifying areas for modification in existing programs 
+        and subsequently developing these modifications. 
+        Writing and implementing efficient code.
+        Determining operational practicality.
+        </p>
+
+    </div>
+      
+    </div>
+  )
+}
+
+export default About
