c8998cead316f574afce2966c34914b152c1ff58
Hargobinda <Mhargobinda@gmail.com>
2023-05-27 11:06:09 +0530

    minor refactor

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,36 +1,24 @@
-import React from 'react'
+import React from 'react';
 
 const About = () => {
   return (
-    <div name="about" className='w-full h-screen bg-gradient-to-b from-gray-800 to-black text-white'>
-    <div className='max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full'>
-     <div className='pb-8'>
-        <p className='text-4xl font-bold inline border-b-4 border-gray-500'>
-        About
+    <div name="about" className="w-full min-h-screen bg-gradient-to-b from-gray-800 to-black text-white">
+      <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
+        <div className="pb-8">
+          <p className="text-2xl md:text-4xl font-bold inline border-b-4 border-gray-500">
+            About
+          </p>
+        </div>
+        <p className="text-base md:text-xl mt-8 md:mt-20">
+          Hello, my name is Hargobinda. I have worked on many projects, and one of my favorite projects that I have worked on is the Crypto Currency Dashboard. Additionally, I have worked on a project called Flash Card Generator.
         </p>
-
-        
-     </div>
-        <p className='text-xl mt-20'> 
-        Hello My Name is Hargobinda i have worked in many project one of my favourite project that i have worked in is Crypto currency Dashboard. 
-        Also i have worked on a Project Flash Card Generator.
+        <br />
+        <p className="text-base md:text-xl mt-4">
+          My responsibilities include researching, designing, implementing, and managing software programs. I also test and evaluate new programs, identify areas for modification in existing programs, and write and implement efficient code. I determine operational practicality and ensure the software meets the desired requirements.
         </p>
-        
-        <br/>
-        <p className='text-xl'>
-        Researching, designing, implementing, 
-        and managing software programs. 
-        Testing and evaluating new programs. 
-        Identifying areas for modification in existing programs 
-        and subsequently developing these modifications. 
-        Writing and implementing efficient code.
-        Determining operational practicality.
-        </p>
-
+      </div>
     </div>
-      
-    </div>
-  )
-}
+  );
+};
 
-export default About
+export default About;
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,44 +1,40 @@
 import React from 'react';
-import HeroImage from  '../Assests/heroImage.png';
-import {MdOutlineKeyboardArrowRight} from 'react-icons/md'
-import {Link} from 'react-scroll';
+import HeroImage from '../Assests/heroImage.png';
+import { MdOutlineKeyboardArrowRight } from 'react-icons/md';
+import { Link } from 'react-scroll';
 
 const Home = () => {
   return (
-    <div name="home" className='h-screen w-full bg-gradient-to-b from-black via-black to-gray-800'>
-        <div className='max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row'>
-            <div className=' flex flex-col justify-center h-full'>
-              <h2 className='text-4xl sm:text-7xl font-bold text-white'>
-                I'm a Frontend Developer
-              </h2>  
-              <p className='  text-gray-500 py-4 max-w-md'>
-                I am new in Frontend Development and designing software. 
-                Currently I love to work on web application using technologies like React,Tailwind, Bootstrap,Node JS and Express JS.
-              </p>
-                 <div>
-                  <Link 
-                   to='portfolio'
-                   smooth
-                   duration={500}
-                   className=' group text-white w=fit px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-r from-cyan-500 to-blue-500 cursor-pointer'>
-                    Portfolio  
-                    <span className='group-hover:rotate-90 duration-300'>
-                      <MdOutlineKeyboardArrowRight size={25} className="ml-1"/>
-                     </span> 
-                  </Link>
-                 </div>
-
-            </div>
-            <div>
-              <img src={HeroImage} alt="my profile" className='rounded-2xl mx-auto ' />
-            </div>
-          
+    <div name="home" className="h-screen w-full bg-gradient-to-b from-black via-black to-gray-800">
+      <div className="max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row">
+        <div className="flex flex-col justify-center h-full">
+          <h2 className="text-3xl sm:text-4xl md:text-7xl font-bold text-white">
+            I'm a Frontend Developer
+          </h2>
+          <p className="text-gray-500 py-4 max-w-md text-sm sm:text-base md:text-lg">
+            I am new to Frontend Development and designing software. Currently, I love to work on web applications using technologies like React, Tailwind CSS, Bootstrap, Node.js, and Express.js.
+          </p>
+          <div>
+            <Link
+              to="portfolio"
+              smooth
+              duration={500}
+              className="group text-white px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-r from-cyan-500 to-blue-500 cursor-pointer text-sm sm:text-base md:text-lg"
+            >
+              Portfolio
+              <span className="group-hover:rotate-90 duration-300">
+                <MdOutlineKeyboardArrowRight size={25} className="ml-1" />
+              </span>
+            </Link>
+          </div>
         </div>
-      
+        <div className="md:pl-4">
+          <img src={HeroImage} alt="my profile" className="rounded-2xl mx-auto md:w-2/3" />
+        </div>
+      </div>
     </div>
-  )
+  );
+};
 
+export default Home;
 
-  }
-
-export default Home
